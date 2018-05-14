51a70d0459904fe2f62d4a6052daa4d120df2a60
jfeudeline <jfeudeline@gmail.com>
2018-05-14 22:16:30 +0200

    improve HTML

--- app.py
+++ app.py
@@ -7,26 +7,46 @@ app = dash.Dash()
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash !'),
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 
-    html.Div(children='''
-    Dash : a web application framework for Python.
-    '''),
+app.layout = html.Div(
+    style={'backgroundColor': colors['background']},
+    children=[
+        html.H1(
+            children='Hello Dash !',
+            style={
+            'textAlign': 'center',
+            'color': colors['text']
+            }),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data' : [
-                {'x' : [1, 2, 3], 'y' : [4, 2, 3], 'type' : 'bar', 'name' : 'SF'},
-                {'x' : [1, 2, 3], 'y' : [6, 4, 1], 'type' : 'bar', 'name' : 'Montréal'}
-            ],
-            'layout' : {
-                'title' : 'Dash Data Visualization'
+        html.Div(
+            children='''
+            Dash : a web application framework for Python.
+            ''',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+
+        dcc.Graph(
+            id='example-graph',
+            figure={
+                'data' : [
+                    {'x' : [1, 2, 3], 'y' : [4, 2, 3], 'type' : 'bar', 'name' : 'SF'},
+                    {'x' : [1, 2, 3], 'y' : [6, 4, 1], 'type' : 'bar', 'name' : 'Montréal'}
+                ],
+                'layout' : {
+                    'title' : 'Dash Data Visualization',
+                    'plot_bgcolor': colors['background'],
+                    'paper_bgcolor': colors['background'],
+                    'font': {'color': colors['text']}
+                    }
             }
-        }
-    )
-])
+        )
+    ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
