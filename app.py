1910c1b8ab73d2d22b9284443b0c135366641544
jfeudeline <jfeudeline@gmail.com>
2018-05-14 22:29:32 +0200

    reusable component bug  correction

--- app.py
+++ app.py
@@ -9,9 +9,27 @@ app = dash.Dash()
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'background': '#EEEEEE',
+    'text': '#222222'
 }
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/'
+    'c78bf172206ce24f77d6363a2d754b59/raw/'
+    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
+    'usa-agricultural-exports-2011.csv')
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
 
 app.layout = html.Div(
     style={'backgroundColor': colors['background']},
@@ -46,32 +64,11 @@ app.layout = html.Div(
                     'font': {'color': colors['text']}
                     }
             }
-        )
+        ),
+
+        html.H4(children='US Agriculture Exports (2011)'), 
+        generate_table(df)
     ])
-
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/'
-    'c78bf172206ce24f77d6363a2d754b59/raw/'
-    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
-    'usa-agricultural-exports-2011.csv')
-
-
-def generate_table(dataframe, max_rows=10):
-    return html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])] +
-
-        # Body
-        [html.Tr([
-            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-        ]) for i in range(min(len(dataframe), max_rows))]
-    )
-
-
-app.layout = html.Div(children=[
-    html.H4(children='US Agriculture Exports (2011)'),
-    generate_table(df)
-])
 
 
 if __name__ == '__main__':
